f10e5e32e781e6b3a4f9b754abb24355027b0bce
Dimas Farias <dimas.farias@dataway.info>
2022-12-26 10:56:02 -0300

    config app center3

--- android/app/src/main/kotlin/com/example/testezure55/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/testezure55/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.testezure55
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 import io.flutter.embedding.android.FlutterActivity
+
+AppCenter.start(getApplication(), "{123456}",
+Analytics.class, Crashes.class);
 
 class MainActivity: FlutterActivity() {
 }
